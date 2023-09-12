6959d882aa39852f631754f0e0357761e2974356
melody-le <tanmui1992@gmail.com>
2023-09-12 10:38:50 +0800

    setup basic stories for image grid

--- src/components/ImageGrid/ImageGrid.stories.tsx
+++ src/components/ImageGrid/ImageGrid.stories.tsx
@@ -12,8 +12,10 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Base: Story = {
-  args: {},
-};
-export const Yellow: Story = {
-  args: {},
+  args: {
+    numberOfImgs: 2,
+    showModal: false,
+    imagesGridWidth: 800,
+    imagesGridHeight: 500,
+  },
 };
--- src/components/ImageGrid/ImageGrid.tsx
+++ src/components/ImageGrid/ImageGrid.tsx
@@ -1,16 +1,35 @@
 export type Props = {};
 
-const ImageGrid = ({}: Props) => {
+interface ImageGridProps {
+  className?: string;
+  numberOfImgs?: number;
+  showModal?: boolean;
+  imagesGridWidth?: number;
+  imagesGridHeight?: number;
+}
+
+const ImageGrid = ({
+  className = "",
+  numberOfImgs = 1,
+  showModal = false,
+  imagesGridWidth,
+  imagesGridHeight,
+}: ImageGridProps) => {
+  const images = [
+    "https://i.pinimg.com/564x/0f/71/0b/0f710bb81c2ff1aff8976239c18acfd2.jpg",
+    "https://i.pinimg.com/564x/57/37/e8/5737e8017b1c6946a6eb25b6db03a72e.jpg",
+    "https://i.pinimg.com/736x/55/ea/08/55ea0881688047362cb6d23f47166b65.jpg",
+    "https://i.pinimg.com/564x/48/40/05/4840054e287358f6b95e6dacfe395f2b.jpg",
+    "https://i.pinimg.com/564x/9c/82/a0/9c82a08a4a0e3b581122ab2576f1c07d.jpg",
+  ];
   return (
     <div
       style={{
-        backgroundColor: "red",
-        borderRadius: "50%",
-        width: 50,
-        height: 50,
+        width: imagesGridWidth,
+        height: imagesGridHeight,
       }}
     >
-      Image Grid
+      <img src={images[0]} alt="first" />
     </div>
   );
 };
