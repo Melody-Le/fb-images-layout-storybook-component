0db563b427e1ede95e8bd8d329d444e84b41e397
melody-le <tanmui1992@gmail.com>
2023-10-01 22:40:28 +0800

    update controll type for numberOfImages

--- src/components/ImageGrid/ImageGridComponent.stories.tsx
+++ src/components/ImageGrid/ImageGridComponent.stories.tsx
@@ -6,7 +6,14 @@ const meta: Meta<typeof ImageGrid> = {
   component: ImageGrid,
   title: "ImageGrid",
   tags: ["autodocs"],
+  argTypes: {
+    numberOfImgs: {
+      control: "select",
+      options: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    },
+  },
 };
+
 export default meta;
 
 type Story = StoryObj<typeof meta>;
